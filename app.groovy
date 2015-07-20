e44d47e012c58cf0ac32684c0f6783128eedad6a
josdem <joseluis.delacruz@gmail.com>
2015-07-20 18:30:08 -0500

    Adding Message listener

--- app.groovy
+++ app.groovy
@@ -1,8 +1,30 @@
+@Grab("spring-boot-starter-hornetq")
+@Grab("hornetq-jms-server")
+import org.hornetq.jms.server.config.impl.JMSQueueConfigurationImpl
+
 @RestController
+@EnableJms
 class MyWebApp {
 
   @RequestMapping(value="/")
   String method(){
-    "Hola Mundo: ${new Date()}"
+    def messageCreator = {session ->
+      session.createTextMessage "Hola Mundo: ${new Date()}"
+    } as MessageCreator
+
+    jmsTemplate.send 'makingdevs', messageCreator
+    "Hola Mundo ${new Date()}"
   }
+}
+
+class Mensajeria {
+
+  @Autowired
+  JmsTemplate jmsTemplate
+
+  @JmsListener(destination = 'makingdevs')
+  def receiver(String message){
+    println message
+  }
+
 }
