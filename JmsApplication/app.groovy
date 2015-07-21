43776a383948894d0667d9ee94ff10d50f378f72
josdem <joseluis.delacruz@gmail.com>
2015-07-21 18:28:36 -0500

    Formateando el texto

--- JmsApplication/app.groovy
+++ JmsApplication/app.groovy
@@ -5,17 +5,17 @@ import org.hornetq.jms.server.config.impl.JMSQueueConfigurationImpl
 @RestController
 class MyWebApp {
 
-    @Autowired
-      JmsTemplate jmsTemplate
+  @Autowired
+  JmsTemplate jmsTemplate
 
-        @RequestMapping(value="/home")
-          String method(){
-                def messageCreater = {session ->
-                        session.createObjectMessage "Hola inmundo en JMS ${new Date()}"
-                            } as MessageCreator
-                    jmsTemplate.send 'makingdevs', messageCreater
-                        "Hola Inmundo ${new Date()}"
-                          }
+  @RequestMapping(value="/home")
+  String method(){
+    def messageCreater = {session ->
+      session.createObjectMessage "Hola inmundo en JMS ${new Date()}"
+    } as MessageCreator
+    jmsTemplate.send 'makingdevs', messageCreater
+    "Hola Inmundo ${new Date()}"
+  }
 }
 
 @EnableJms
@@ -23,22 +23,22 @@ class MyWebApp {
 @Log
 class Mensajeria {
 
-    @Autowired
-      String myString
+  @Autowired
+  String myString
 
-        @JmsListener(destination = 'makingdevs')
-          def recibidorZ(String message){
-                log.info myString
-                    log.info message
-                      }
+  @JmsListener(destination = 'makingdevs')
+  def recibidorZ(String message){
+    log.info myString
+    log.info message
+  }
 
-      @Bean JMSQueueConfigurationImpl springBootQueue() {
-            new JMSQueueConfigurationImpl('makingdevs', null, false)
-              }
+  @Bean JMSQueueConfigurationImpl springBootQueue() {
+    new JMSQueueConfigurationImpl('makingdevs', null, false)
+  }
 }
 
 beans {
-    myString String, "Hola bean de spring"
+  myString String, "Hola bean de spring"
 }
 
 
